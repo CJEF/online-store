c7a92f52dc9415463091b6d6b1db1602ed57d46e
CJEF <ef2200@mail.ru>
2021-05-06 15:55:46 +0600

    почти фулл десктоп

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,16 @@
 // accordion.addEventListener("click", function() {
 //   accordion.classList.toggle("accordion--active");
 // });
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+}
